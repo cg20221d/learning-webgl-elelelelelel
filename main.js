62798f3b2881151c071fa589faaa0f8d054620c4
unknown <nelqolby@gmail.com>
2022-09-07 15:21:51 +0700

    tambah

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ function main(){
     var vertexShaderCode =
     "void main() {" +
     "}";
+    var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShaderObject, vertexShaderCode);
+    gl.compileShader(vertexShaderObject); //sampai sini udah jadi .o
 
     // Fragment shader
     var fragmentShaderCode = `
@@ -13,4 +16,21 @@ function main(){
 
     }
     `;
+    var fragmentShaderObject = gl.createShader (gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShaderObject, fragmentShaderCode);
+    gl.compileShader(fragmentShaderObject); //sampai sini sudah jadi .o
+
+    var shaderProgram =(gl.createProgram); //wadah dari executable (.exe)
+    gl.attackShader(shaderProgram, vertexShaderObject);
+    gl.attackShader(shaderProgram, fragmentShaderObject);
+    gl.linkProgram(shaderProgram);
+    gl.useProgram(shaderProgram); //<3
+    //kalau kamu ada gl lebih dari 1, <3 dibuat jadi gl1 gl2
+
+    gl.clearColor(1.0,   0.65,   0.0,   1.0);
+    //           Merah   Hijau   Biru   Transparan
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+
+    // di vartex sama fragment itu gaya nulisnya doang yg beda
 }
