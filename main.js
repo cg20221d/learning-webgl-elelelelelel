d61c4fc0fe1df5a51b365ec089accd116e37a4c4
unknown <nelqolby@gmail.com>
2022-09-07 14:59:12 +0700

    tambahin

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 function main(){
-    
+    var kanvas = document.getElementById("kanvas");
+    var gl = kanvas.getContext("webgl");
+
+    // Vertex shader => nanti kita pake untuk mendeklarasikan string
+    var vertexShaderCode =
+    "void main() {" +
+    "}";
+
+    // Fragment shader
+    var fragmentShaderCode = `
+    void main() {
+
+    }
+    `;
 }
